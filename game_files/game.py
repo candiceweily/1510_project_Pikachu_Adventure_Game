6cee85ed19265d5649e1d4f5c566567856ade78d
Melissa Shao <mshao8@my.bcit.ca>
2024-04-11 14:13:38 -0700

    Rewrite doctest for make_character and describe_current_location

--- game_files/game.py
+++ game_files/game.py
@@ -62,10 +62,12 @@ def make_character() -> dict[str: str | int]:
     :postcondition: correctly create the character for the game
     :return: the character as a dictionary
 
-    # >>> player_character = make_character()
-    # >>> player_character
-    # {'Name': 'Pikachu', 'X-coordinate': 0, 'Y-coordinate': 0, 'Level': 1, 'Current HP': 20, 'Max HP': 20, 'XP': 0,
-    # 'Ability': 'Thunder Shock', 'Potions': 5}
+    >>> player_character = make_character()
+    >>> player_character == {
+    ... 'Name': 'Pikachu', 'X-coordinate': 0, 'Y-coordinate': 0, 'Level': 1,
+    ... 'Current HP': 20, 'Max HP': 20, 'XP': 0, 'Ability': 'Thunder Shock',
+    ... 'Potions': 5}
+    True
     """
     return {
         "Name": "Pikachu",
@@ -112,17 +114,22 @@ def describe_current_location(board: dict[tuple[int, int]: str], character: dict
     :precondition: character is a non-empty dictionary that contains location and attributes
     :postcondition: correctly describe the current location of the character
 
-    # >>> player_board = {(0, 0): 'Kanto', (0, 1): 'Johto', (1, 0): 'Hisui', (1, 1): 'Sinnoh'}
-    # >>> player_character = {'Name': 'Pikachu', 'X-coordinate': 0, 'Y-coordinate': 0, 'Level': 1, 'Current HP': 20,
-    # 'Max HP': 20, 'XP': 0, 'Ability': 'Thunder Shock', 'Potions': 5}
-    # >>> describe_current_location(player_board, player_character)
-    # You are in Kanto, at location (0, 0).
-    #
-    # >>> player_board = {(0, 0): 'Kanto', (0, 1): 'Johto', (1, 0): 'Hisui', (1, 1): 'Sinnoh'}
-    # >>> player_character = {'Name': 'Pikachu', 'X-coordinate': 1, 'Y-coordinate': 0, 'Level': 1, 'Current HP': 20,
-    # 'Max HP': 20, 'XP': 0, 'Ability': 'Thunder Shock', 'Potions': 5}
-    # >>> describe_current_location(player_board, player_character)
-    You are in Hisui, at location (1, 0).
+    >>> player_board = {(0, 0): 'Kanto', (0, 1): 'Johto', (1, 0): 'Hisui', (1, 1): 'Sinnoh'}
+    >>> player_character = {
+    ... 'Name': 'Pikachu', 'X-coordinate': 0, 'Y-coordinate': 0,
+    ... 'Level': 1, 'Current HP': 20, 'Max HP': 20, 'XP': 0,
+    ... 'Ability': 'Thunder Shock', 'Potions': 5
+    ... }
+    >>> describe_current_location(player_board, player_character)
+    You are in Kanto, at location (0, 0).
+    >>> player_board = {(0, 0): 'Kanto', (0, 1): 'Johto', (1, 0): 'Hisui', (1, 1): 'Sinnoh'}
+    >>> player_character = {
+    ... 'Name': 'Pikachu', 'X-coordinate': 1, 'Y-coordinate': 1,
+    ... 'Level': 1, 'Current HP': 20, 'Max HP': 20, 'XP': 0,
+    ... 'Ability': 'Thunder Shock', 'Potions': 5
+    ... }
+    >>> describe_current_location(player_board, player_character)
+    You are in Sinnoh, at location (1, 1).
     """
     location = (character["X-coordinate"], character["Y-coordinate"])
     print(f'You are in {board[location]}, at location {location}.')
