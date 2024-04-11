19cdfe1f4e13e29026d5d9d9cd0fc2527fb6f70a
Melissa Shao <mshao8@my.bcit.ca>
2024-04-10 20:38:28 -0700

    Write doctest for check_level_up function

--- game_files/game.py
+++ game_files/game.py
@@ -347,6 +347,13 @@ def check_level_up(character: dict[str: str | int]) -> None:
     :param character: character is a non-empty dictionary
     :precondition: character is a non-empty dictionary that contains location and attributes
     :postcondition: check if Pikachu has enough XP to level up and correctly update the level and ability
+
+    >>> test_character = {'Level': 1, 'XP': 10, 'Max HP': 20, 'Current HP': 20}
+    >>> check_level_up(test_character)
+    Pikachu has leveled up to Level 2! Ability upgrades to Thunder Shower.
+    >>> test_character = {'Level': 2, 'XP':30, 'Max HP': 30, 'Current HP': 30}
+    >>> check_level_up(test_character)
+    Pikachu has leveled up to Level 3! Ability upgrades to Thunder Storm.
     """
     level = character["Level"]
     if level < 3 and character["XP"] >= LEVEL_UP_EXPERIENCE[level - 1]:
