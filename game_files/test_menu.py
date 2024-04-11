eacde9e21225fe0cc750349705cb6d3d6d098f12
Melissa Shao <mshao8@my.bcit.ca>
2024-04-10 22:19:20 -0700

    Write unit test for menu function

---
+++ game_files/test_menu.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from unittest.mock import patch
+from game_files.game import menu
+
+
+class TestMenu(TestCase):
+    @patch('builtins.input', side_effect=['4', '1'])
+    @patch('builtins.print')
+    def test_menu_first_invalid_then_valid_input(self, _, __):
+        choice = menu()
+        self.assertEqual(choice, '1')
+
+    @patch('builtins.input', return_value='1')
+    @patch('builtins.print')
+    def test_start_game_choice(self, _, __):
+        self.assertEqual(menu(), '1')
+
+    @patch('builtins.input', return_value='2')
+    @patch('builtins.print')
+    def test_help_choice(self, _, __):
+        self.assertEqual(menu(), '2')
+
+    @patch('builtins.input', return_value='3')
+    @patch('builtins.print')
+    def test_exit_game_choice(self, _, __):
+        self.assertEqual(menu(), '3')
