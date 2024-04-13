8e4293115e9f84994494f1807e788d0e5af62684
Candice Wei <weily00@bu.edu>
2024-04-13 09:18:52 -0700

    Remove unused import statements

--- game_files/game.py
+++ game_files/game.py
@@ -1,19 +1,15 @@
-import random
-
-from game_files import ROWS, COLUMNS, LEVEL_UP_EXPERIENCE, MAX_HP_BY_LEVEL, FOE_NAMES, WIN_CHANCE_BY_LEVEL
-from game_files import HP_CHANGE_BY_LEVEL, XP_CHANGE_BY_LEVEL, ABILITY_BY_LEVEL
-from game_files import BOSS_BATTLE_XP_NEED, BOSS_BATTLE_HP_REDUCE, BOSS_BATTLE_HIT_CHANCE
+from game_files import ROWS, COLUMNS
+from game_files import BOSS_BATTLE_XP_NEED
 
 from game_files.game_menu import menu, display_help
 from game_files.game_board import make_board
 from game_files.game_character import make_character, describe_current_location
 from game_files.game_navigation import display_map_with_character_position, validate_move, move_character
-from game_files.game_progress import get_user_choice, decide_use_potion
-from game_files.game_foe import handle_regular_foe_encounter, calculate_battle_outcome
-from game_files.game_art import display_welcome_message, print_congratulations
-from game_files.game_mini_games import run_math_quiz, run_treasure_hunt
+from game_files.game_progress import get_user_choice
+from game_files.game_art import display_welcome_message
 from game_files.game_level import check_level_up
 from game_files.game_encounters import check_for_encounters
+from game_files.game_final_boss import final_boss_battle
 
 
 def game() -> None:
